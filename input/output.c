ad677e597bc8d06d7b647fd6e13a2bb9ee53ad04
Shakti_Singh_@195 <147183484+Shakti-195@users.noreply.github.com>
2024-12-21 22:45:01 +0530

    Update Variables,Data Types and input/output

---
+++ input/output.c
@@ -0,0 +1,52 @@
+//program 1
+
+//write a program to calculate perimeter of rectangle take a&b ,from the user 
+
+#include <stdio.h>
+int main(){
+  int a,b;
+  printf("enter the value of a\n");
+  scanf("%d",&a);
+  printf("enter the value of b\n");
+  scanf("%d",&b);
+  printf("perimeter of rectangle of a and b is %d ",2*(a+b));
+  return 0;
+}
+
+//program 2
+
+//take a number(n) from user and output its cube(n*n*n);
+#include <stdio.h>
+int main(){
+  int n;
+  printf("enter a number :\n ");
+  scanf("%d",&n);
+  printf("cube of %d is %d",n,n*n*n);
+  return 0;
+}
+
+//program 3
+
+//write a program to calculate area of a square (s8de is given)
+#include <stdio.h>
+int main(){
+  int side;
+  printf("enter side : ");
+  scanf("%d",&side);
+  printf("area of square is %d",side *side);
+  return 0;
+  
+}
+
+//program 4
+
+//write a program to calculate area of a circle 
+
+#include <stdio.h>
+int main(){
+  int r;
+  printf("enter the radius of the circle : ");
+  scanf("%d",&r);
+  printf("the area of the circle is : %f",3.14*r*r);
+  return 0;
+}
